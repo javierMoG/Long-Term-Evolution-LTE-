c5613863a8433112a2153bf4074fd269a2b695e1
Alexis Calvillo <acalvillm@gmail.com>
2019-12-06 15:54:32 -0600

    Comentarios

--- simulacion.m
+++ simulacion.m
@@ -1,23 +1,33 @@
 %Parámetros de simulación
-nFr=20;
+tic
+%Número de frames simulados
+nFr=100;
+%Espaciación entre valores del SNR
 espSnr=.5;
+%Modulaciones a simular
 modulacion=["16QAM";"64QAM";"QPSK"];
+%Vector de SNR con los valores del SNR simulado
 SNRIn=[];
+%Vector de throughput con los valores del throughput simulado (en Mbps)
 trgh=[];
+%Vector de Bit Error Rate con los valores de Bit Error Rate simulado
 BER=[];
 for i=1:length(modulacion)
     [SNRIn(i,:),BER(i,:),trgh(i,:)]=PHY(modulacion(i),nFr,espSnr,false);
 end
+toc
+
+%Gráfica de resultados de Throughput
 figure(1)
 plot(SNRIn(1,:),trgh(1,:),'*-.')
 grid on;
 hold on;
 plot(SNRIn(2,:),trgh(2,:),'*-.')
 plot(SNRIn(3,:),trgh(3,:),'*-.')
-%plot(SNRIn, 1e-6*simThroughput/(NFrames*10e-3),'*-.');
 xlabel('SNR (dB)');
 ylabel('Throughput (Mbps)');
 legend(modulacion,'Location','NorthWest');
+%Gráfica de resultados de BER
 figure(2)
 plot(SNRIn(1,:), BER(1,:));
 grid on;
