388b830d8637e1bd09d2c2b4aae62cef48a5c35f
javierMoG <javiermg19978@gmail.com>
2019-12-06 19:15:55 -0600

    Add files via upload

--- hNewHARQProcess.m
+++ hNewHARQProcess.m
@@ -1,11 +1,12 @@
 % hNewHARQProcess Initialize HARQ processes
 %
-%   HARQPROCESSES = hNewHARQProcess(ENBUE) is a structure containing
-%   the initialized parameters of hybrid automatic repeat request processes
-%   given a downlink, uplink or sidelink configuration ENBUE.
+%   HARQPROCESSES = hNewHARQProcess(ENBUE) es una estructura que contiene
+%   los parametros incializados de los procesos HARQ (Hybrid Automatic 
+%   Repeat Request) dado una configuracion ENBUE de enlace descendente, 
+%   ascendente o enlance lateral
 %
-%   HARQPROCESSES = hNewHARQProcess(ENB,PDSCH) initializes HARQ processes
-%   for a given ENB structure and PDSCH configuration structure. 
+%   HARQPROCESSES = hNewHARQProcess(ENB,PDSCH) inicializa los procesos HARQinitializes HARQ processes
+%   para una estructura ENB y configuracion de estructura PDSCH dados. 
 
 %   Copyright 2015-2018 The MathWorks, Inc.
 
@@ -14,35 +15,36 @@ function harqProcesses = hNewHARQProcess(enbue,varargin)
     if(nargin>1)
         enbue.PDSCH = varargin{1};
     end
-    % Identify the structure containing HARQ entity parameters (PDSCH in
-    % downlink, PUSCH in uplink, UE in sidelink)
+    % Identifica la estructura que contiene la entidad de parametros HARQ
+    % (PDSCH en enlace descente, PUSCH en enlace ascendente, UE en enlace
+    % lateral)
     if isfield(enbue,'PDSCH')
         harqstruct = enbue.PDSCH;
     elseif isfield(enbue,'PUSCH')
         harqstruct = enbue.PUSCH;
     else
-        harqstruct = enbue; % Input structure is 'UE'
+        harqstruct = enbue; % La estructura de entrada es 'UE'
     end
     
-    % If Modulation is a cell array multiple codewords are used 
+    % Si la Modulacion es un arreglo de celdad multiples palabras codigo
+    % son usadas
     if (iscell(harqstruct.Modulation)) || isstring(harqstruct.Modulation)
         ncw = length(harqstruct.Modulation);
     else
         ncw = 1;
     end
-    harqProcess.data = {};                     % Initialize data
-    harqProcess.iniConfig = harqstruct;        % Initialization config
-    harqProcess.iniConfig.RVIdx = ones(1,ncw); % Add RVIdx to config
-    harqProcess.blkerr = zeros(1,ncw);         % Initialize block errors
-    harqProcess.ncw = ncw;                     % Set number of codewords
+    harqProcess.data = {};                     % Inicializamos la informacion
+    harqProcess.iniConfig = harqstruct;        % Inicializamos la configuracion
+    harqProcess.iniConfig.RVIdx = ones(1,ncw); % Add RVIdx to config Agregamos RVIdx a la configuracion
+    harqProcess.blkerr = zeros(1,ncw);         % Inicializamos el bloque de errores
+    harqProcess.ncw = ncw;                     % Establecemos el numero de palabras codigo
     
-    % Initialize transmit configuration structure
+    % Inicializamos la configuracion de la estructira transmitir
     harqProcess.txConfig = harqProcess.iniConfig;
     
-    % Create an empty structure for each codeword
+    % Creamos una estructura vacia para cada palabra codigo
     harqProcess.decState(1:ncw) = deal(struct());
     
-    % Create HARQ processes as indicated by NHARQProcesses
+    % Creamos los procesos HARQ como los indica NHARQProcesses
     harqProcesses(1:harqstruct.NHARQProcesses) = harqProcess;
-    
 end
--- hPlotRVSequence.m
+++ hPlotRVSequence.m
@@ -1,38 +1,38 @@
-% hPlotRVSequence Plots RV sequence history 
-%   hPlotRVSequence(SNRIN,RVSEQPTRHISTORY,NFRAMES) plots the contents of
-%   RVSEQPTRHISTORY for a number of SNR points specified in the input
-%   vector SNRIN. NFRAMES specifies the number of frames to consider,
-%   RVSEQPTRHISTORY is a cell array with one entry per SNR point. The
-%   history of the values of the RV sequence pointer history is stored in a
-%   matrix for the corresponding SNR point. This matrix can have one or two
-%   rows for the one and two codeword case respectively. Each column of
-%   this matrix stores the corresponding RV sequence pointer value for a
-%   specific subframe.
+% hPlotRVSequence gráfica el historial de la secuencia RV
+%   hPlotRVSequence (SNRIN,RVSEQPTRHISTORY,NFRAMES) grafica los contenidos
+%   RVSEQPTRHISTORY para un numero de puntos SNR especificados en el
+%   vector entrada SNRIN. NFRAMES especifica el numero de tramas a
+%   considerar, RVSEQPTHISTORY es una celda de arreglos con una entrada por
+%   punto SNR. La historia de los valores de la historia de la secuencia de
+%   apuntadores RV se guarda en una matrix para el correspondiente punto de 
+%   SNR. Esta matrix puede tener una o dos filas para el caso de una o dos
+%   palabras código respectivamente. Cada columna de está matrix guarda la
+%   correspondiente secuencia de valores de apuntadores RV para una
+%   subtrama especifica.
 %
-%   The history for each SNR point is plotted in a different figure. 
-
+%   La historia de cada punto SNR se imprime en una figura diferente.
 %   Copyright 2016 The MathWorks, Inc.
 
 function hPlotRVSequence(SNRIn,allRvSeqPtrHistory,NFrames)
 
-% get number of codewords
+% obtiene el numero de palabras codigo
 ncw = size(allRvSeqPtrHistory{1},1);
 
 for snrIdx = 1:numel(SNRIn)
     SNRdB = SNRIn(snrIdx);
     rvSeqPtrHistory = allRvSeqPtrHistory{snrIdx};
-    % plot history for first codeword
+    % imprime la historia para la primera palabra codigo
     figure;
     line((0:(NFrames*10-1)),rvSeqPtrHistory(1,:),'Color','k',...
         'Marker','o','LineStyle','none')
-    ax1 = gca; % current axes
+    ax1 = gca; % ejes actuales
     ax1.YLabel.String = 'cw1  RV sequence index';
     ax1.XLabel.String = 'subframe';
     ax1.YLim = [min(rvSeqPtrHistory(:))-1 max(rvSeqPtrHistory(:))+1];
     ax1.YTick = [0 1 2 3 4 5];
-    % plot history for second codeword if it exists
+    %imprime la historia para la segunda palabra codigo si existe
     if ncw == 2
-        ax1_pos = ax1.Position; % position of first axes
+        ax1_pos = ax1.Position; % posicion del primer eje
         ax2 = axes('Position',ax1_pos,...
             'YAxisLocation','right','Color','none','YColor','r');
         ax2.YLabel.String = 'cw2  RV sequence index';
